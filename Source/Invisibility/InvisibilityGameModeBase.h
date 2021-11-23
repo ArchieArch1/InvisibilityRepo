8711c9a4c8b83f634d46d11fac55502729bee074
Archie <35892559+ArchieArch1@users.noreply.github.com>
2021-11-23 12:56:39 +0000

    Invisibility functionality added
    Invis & sound added.
    Translucent material is an issue.

---
+++ Source/Invisibility/InvisibilityGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InvisibilityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVISIBILITY_API AInvisibilityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
